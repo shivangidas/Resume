35f9a4930988416853028a49ea7e6e325111de05
Shivangi Das <shivangidas@users.noreply.github.com>
2017-04-09 19:29:47 +0530

    Add files via upload
    Page loading added

--- js/common.js
+++ js/common.js
@@ -1,3 +1,7 @@
+  $(window).load(function() {
+        $(".bg_load").fadeOut("slow");
+        $(".wrapper").fadeOut("slow");
+    });
 $(document).ready(function() {
     "use strict";
     $('[data-toggle="tooltip"]').tooltip();
